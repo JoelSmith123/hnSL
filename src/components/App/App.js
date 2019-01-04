9752a44983eecf40d507cccf5a28b10f26671ed9
Joel Smith <joelwsmith123@gmail.com>
2019-01-04 10:59:08 -0700

    Set up SuggestionsSidebar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import SuggestionsSidebar from '../../containers/SuggestionsSidebar/SuggestionsSidebar.js'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <SuggestionsSidebar />
       </div>
     );
   }
 }
 
-export default App;
