53ee3d41575dc3e9f9ed509749c9324275affe92
Joel Smith <joelwsmith123@gmail.com>
2019-01-04 11:17:01 -0700

    Set up SuggestionCard

--- src/containers/SuggestionsSidebar/SuggestionsSidebar.js
+++ src/containers/SuggestionsSidebar/SuggestionsSidebar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import SuggestionCard from '../SuggestionCard/SuggestionCard.js'
 import './SuggestionsSidebar.css'
 
 export default class SuggestionsSidebar extends Component {
   render() {
     return (
       <div className='SuggestionsSidebar'>
+        <SuggestionCard />
       </div>
     )
   }
