14914284278a671355d43f3a233c749ef94978f7
Joel Smith <joelwsmith123@gmail.com>
2019-01-04 12:06:46 -0700

    Set up Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import './App.css'
 import SuggestionsSidebar from '../../containers/SuggestionsSidebar/SuggestionsSidebar.js'
 import SignIn from '../../containers/SignIn/SignIn.js'
+import Header from '../../containers/Header/Header.js'
+
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <SuggestionsSidebar />
         <SignIn />
       </div>
---
+++ src/containers/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className='Header'>
+        <h2 className='header-title'>Welcome to hnSL!</h2>
+      </header>
+    )
+  }
+}
