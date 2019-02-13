a9ee69f95129684344bd3c8aebca4aafcd59bc09
Joel Smith <joelwsmith123@gmail.com>
2019-02-13 12:42:46 -0700

    Add heroku configuration

---
+++ build/precache-manifest.19b43d621a715e0d1b388a71c08e111c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e0d857f9c2036022fea8",
+    "url": "/static/js/main.e0d857f9.chunk.js"
+  },
+  {
+    "revision": "260c39a095ff3aa433e9",
+    "url": "/static/js/1.260c39a0.chunk.js"
+  },
+  {
+    "revision": "e0d857f9c2036022fea8",
+    "url": "/static/css/main.1fe6d3ab.chunk.css"
+  },
+  {
+    "revision": "0ae0c6a5c35e5bf6449cf9e5ba2551cd",
+    "url": "/index.html"
+  }
+];
