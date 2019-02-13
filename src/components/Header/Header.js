0b437a9f929c8eacfeb06d291e3403818c56f8ab
Joel Smith <joelwsmith123@gmail.com>
2019-02-13 12:22:46 -0700

    Add name changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
   render() {
     return (
       <header className='Header'>
-        <NavLink to='/'><h2 className='header-title'>Welcome to hnSL!</h2></NavLink>
+        <NavLink to='/'><h2 className='header-title'>Welcome to suggestME!</h2></NavLink>
       </header>
     )
   }
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -6,8 +6,8 @@ export default class Welcome extends Component {
   render() {
     return (
       <div className='Welcome'>
-        <h2 className='welcome-title' >hnSL suggests games, music, movies, TV shows, books, and authors based on the things you already like.</h2>
-        <NavLink to='/filter'><button className='welcome-btn'>Try hnSL!</button></NavLink>
+        <h2 className='welcome-title' >suggestME suggests games, music, movies, TV shows, books, and authors based on the things you already like.</h2>
+        <NavLink to='/filter'><button className='welcome-btn'>Try suggestME!</button></NavLink>
       </div>
     )
   }
